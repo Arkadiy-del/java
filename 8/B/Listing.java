8f7c64266c1dfd459bbb64099ced179f42cdd129
Arkadiy-del <82815983+Arkadiy-del@users.noreply.github.com>
2026-05-15 03:30:48 +0800

    Create Listing.java

---
+++ 8/B/Listing.java
@@ -0,0 +1,22 @@
+package org.example;
+
+class Listing {
+    private String code;
+
+    public Listing(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return code;
+    }
+}
